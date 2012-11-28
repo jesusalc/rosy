f2cf9a79ff48bdb5c7e9235881cb3dd8fd253ade
Tim Wood <washwithcare@gmail.com>
2012-11-28 14:52:29 -0800

    Adding model class to Rosy

---
+++ rosy/models/Model.js
@@ -0,0 +1,64 @@
+define(
+
+	[
+		"../base/Class"
+	],
+
+	function (DOMClass) {
+
+		"use strict";
+
+		return DOMClass.extend({
+
+			__data : null,
+
+			init : function (input) {
+				this.__data = {};
+
+				this.set(input);
+			},
+
+			set : function (key, val) {
+				var i;
+
+				if (typeof key === "string") {
+					if (this.__data[key] !== val) {
+						this.__data[key] = val;
+						this.trigger('change:' + key);
+					}
+				} else {
+					for (i in key) {
+						this.set(i, key[i]);
+					}
+				}
+			},
+
+			get : function (key) {
+				var i, output;
+
+				if (key) {
+					return this.__data[key];
+				}
+
+				output = {};
+
+				for (i in this.__data) {
+					output[i] = this.__data[i];
+				}
+
+				return output;
+			},
+
+			keys : function () {
+				var output = [],
+					i;
+
+				for (i in this.__data) {
+					output.push(i);
+				}
+
+				return output;
+			}
+		});
+	}
+);
