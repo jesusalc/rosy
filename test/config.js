6e3e4e2840b75d4faf31f62098152b974164910d
Christian Harden <christian.harden@gmail.com>
2013-03-04 20:01:19 -0800

    fixed refs for oxblood testing

--- test/config.js
+++ test/config.js
@@ -1,22 +1,10 @@
 require.config({
 	paths : {
+		"rosy" : "/rosy",
+		"$plugin" : "/rosy/plugins/jquery-plugin/jquery-plugin",
+		"jsonFile" : "/rosy/plugins/json-file/jsonFile",
 		"core" : "../test/core",
 		"models" : "../test/models",
 		"routing" : "../test/routing"
 	}
-});
-
-require([
-	"core/Initialization",
-	"core/Notifications",
-	"core/PseudoEvents",
-	"core/ExternalLibs",
-	"core/Inheritance",
-	"core/Destruction",
-	"core/Timers",
-	"core/Scope",
-	"models/Model",
-	"routing/Routing"
-], function () {
-
 });
---
+++ test/tests.js
@@ -0,0 +1,14 @@
+define([
+	"core/Initialization",
+	"core/Notifications",
+	"core/PseudoEvents",
+	"core/ExternalLibs",
+	"core/Inheritance",
+	"core/Destruction",
+	"core/Timers",
+	"core/Scope",
+	"models/Model",
+	"routing/Routing"
+], function () {
+	return true;
+});
