cd11cabe9d8b8b50724eb702c9ac110dc18de7cc
Richard Herrera <rich@doctyper.com>
2013-04-11 12:30:12 -0700

    Update paths.

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 require.config({
 
 	paths : {
-		"$" : "jquery/jquery",
-		"json" : "json3/lib/json3",
-		"Cookies" : "cookies/index",
-		"text" : "requirejs-text/text",
-		"Handlebars" : "handlebars.js/dist/handlebars",
+		"$" : "../jquery/jquery",
+		"json" : "../json3/lib/json3",
+		"Cookies" : "../cookies/index",
+		"text" : "../requirejs-text/text",
+		"Handlebars" : "../handlebars.js/dist/handlebars",
 		"templates" : "../../templates",
-		"rosy" : "rosy/src",
-		"jsonFile" : "rosy/src/plugins/json-file/jsonFile",
-		"$plugin" : "rosy/src/plugins/jquery-plugin/jquery-plugin"
+		"rosy" : "src",
+		"jsonFile" : "src/plugins/json-file/jsonFile",
+		"$plugin" : "src/plugins/jquery-plugin/jquery-plugin"
 	},
 
 	waitSeconds : 15,
